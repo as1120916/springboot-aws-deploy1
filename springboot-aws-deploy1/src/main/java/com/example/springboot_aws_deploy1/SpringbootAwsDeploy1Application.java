a5c8ff5644b702b23ef40ad608b4ea7071a3e886
Akash Singh <your-github-email@example.com>
2026-06-03 11:19:23 +0000

    Initial commit

---
+++ springboot-aws-deploy1/src/main/java/com/example/springboot_aws_deploy1/SpringbootAwsDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot_aws_deploy1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsDeploy1Application.class, args);
+	}
+
+}
